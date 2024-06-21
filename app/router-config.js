c9e9bac33783f5291e03ea3a70245ab5e9d86f6f
Seth Baxter <TheWarrior0216@gmail.com>
2024-06-21 10:57:54 -0600

    Beggining --Finally

---
+++ app/router-config.js
@@ -0,0 +1,15 @@
+import { NotesController } from "./controllers/NotesController.js";
+import { Router } from "./utils/Router.js";
+
+
+export const router = new Router([
+  {
+    path: '',
+    controllers: [NotesController],
+    view: ``
+  },
+  {
+    path: '#/about',
+    view: 'app/views/AboutView.html'
+  }
+])
