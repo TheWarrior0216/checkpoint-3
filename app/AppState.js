74b7c70af1d7674a0828896565e891127c2212ba
Seth Baxter <TheWarrior0216@gmail.com>
2024-06-21 11:21:56 -0600

    Setup note data types

--- app/AppState.js
+++ app/AppState.js
@@ -1,10 +1,21 @@
+import { Note } from "./models/Note.js"
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
 
-  /**@type {import('./models/Example.js').Example[]} */
-  examples = []
+
+  /**@type {import('./models/Note.js').Note[]} */
+  Notes = [
+    new Note({
+      title: 'Javascript',
+      body: 'lorem ipsum',
+    }),
+    new Note({
+      title: 'Oh No OH no',
+      body: 'lorem ipsum something something something',
+    }),
+  ]
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
--- app/models/Note.js
+++ app/models/Note.js
@@ -1,3 +1,11 @@
-class Note {
-
+export class Note {
+  constructor(data) {
+    this.title = data.title
+    this.createdAt = new Date()
+    this.updatedAt = new Date()
+    this.body = data.body
+  }
+  get noteTemplate() {
+    return ``
+  }
 }
